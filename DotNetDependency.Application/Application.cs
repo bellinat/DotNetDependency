7ed4b6ad67553a136cb86fb4e4cff05419b35558
Eduardo Novaes Taveira Bellinat <eduardo.bellinat@ciashop.com.br>
2017-08-17 16:00:03 -0300

    Initial project

---
+++ DotNetDependency.Application/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDependency.Application
+{
+    public class Application
+    {
+        public Application()
+        {
+            var rep = new Repository.Repository();
+        }
+    }
+}
